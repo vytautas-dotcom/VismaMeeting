afeb3834917fb94ef4e759a5fd206024dcbd4d5b
vytautas-dotcom <tumas.vytautas@gmail.com>
2022-05-12 20:33:15 +0300

    Classes for employees

---
+++ VismaMeeting/VismaMeeting/Employees/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace VismaMeeting.Employees
+{
+    internal class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        //Dictionary contains Meeting's id and date when person was added
+        public Dictionary<Guid, DateTime> PersonMeetings { get; set; }
+    }
+}
---
+++ VismaMeeting/VismaMeeting/Employees/PersonList.cs
@@ -0,0 +1,8 @@
+﻿namespace VismaMeeting.Employees
+{
+    internal class Employees : List<Person>
+    {
+        public void Add(Guid id, string name, Dictionary<Guid, DateTime> meetingDate)
+            => Add(id, name, meetingDate);
+    }
+}
