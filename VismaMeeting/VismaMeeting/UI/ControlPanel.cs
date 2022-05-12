5ce1bd6324014c94d2838c58161882eeb5d2d089
vytautas-dotcom <tumas.vytautas@gmail.com>
2022-05-13 01:21:32 +0300

    Create user

---
+++ VismaMeeting/VismaMeeting/UI/ControlPanel.cs
@@ -0,0 +1,26 @@
+﻿using VismaMeeting.Employees;
+using VismaMeeting.MeetingData;
+using VismaMeeting.Serialization;
+using VismaMeeting.Users;
+
+namespace VismaMeeting.UI
+{
+    internal class ControlPanel
+    {
+        private readonly CreateUser _createUser;
+        private readonly MeetingSerialazer _meetingSerialazer;
+        private readonly PersonSerialazer _personSerialazer;
+        public MeetingList MeetingList { get; set; }
+        public PersonList PersonList { get; set; }
+        public User User { get; set; }
+        public ControlPanel()
+        {
+            _meetingSerialazer = new MeetingSerialazer();
+            _personSerialazer = new PersonSerialazer();
+            MeetingList = _meetingSerialazer.Deserialize();
+            PersonList = _personSerialazer.Deserialize();
+            _createUser = new CreateUser(PersonList, _personSerialazer);
+            User = new UserPerson(_createUser.SelectUser());
+        }
+    }
+}
---
+++ VismaMeeting/VismaMeeting/UI/CreateUser.cs
@@ -0,0 +1,32 @@
+﻿using VismaMeeting.Employees;
+using VismaMeeting.Functions;
+using VismaMeeting.Serialization;
+
+namespace VismaMeeting.UI
+{
+    internal class CreateUser
+    {
+        private readonly PersonList _personList;
+        private readonly PersonSerialazer _personSerialazer;
+        private readonly PersonShowData _personShowData;
+
+        public CreateUser(PersonList personList, PersonSerialazer personSerialazer)
+        {
+            _personList = personList;
+            _personSerialazer = personSerialazer;
+            _personShowData = new PersonShowData();
+            _personList = _personSerialazer.Deserialize();
+            _personShowData.ShowNamesIndexes(_personList);
+        }
+        public Person SelectUser()
+        {
+            Person person = new Person();
+            Console.WriteLine("Enter number which user you are");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            person = _personList[userNumber];
+            Console.WriteLine(new String('-', 50));
+            Console.WriteLine($"Hello {person.Name}, select function please");
+            return person;
+        }
+    }
+}
