6526e6042aa33d19e395aa76e5ef0ef9b2637f7c
vytautas-dotcom <tumas.vytautas@gmail.com>
2022-05-17 15:19:45 +0300

    Get from json, save to json service

---
+++ VismaMeeting/VismaMeeting_v2/Services/DataServices/DbService.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace VismaMeeting_v2.Services.DataServices
+{
+    public class DbService<T> : IDbService<T> where T : new()
+    {
+        public T Get()
+        {
+            T list = new();
+            string fileName = typeof(T).Name.ToLower();
+            using (StreamReader streamReader = new StreamReader($"../../../Data/{fileName}.json"))
+            {
+                string json = streamReader.ReadToEnd();
+                list = JsonConvert.DeserializeObject<T>(json);
+            }
+            return list;
+        }
+
+        public void Save(T list)
+        {
+            string fileName = typeof(T).Name.ToLower();
+            using (StreamWriter jsonStream = File.CreateText($"../../../Data/{fileName}.json"))
+            {
+                var jss = new JsonSerializer();
+                jss.Serialize(jsonStream, list);
+            }
+        }
+    }
+}
---
+++ VismaMeeting/VismaMeeting_v2/Services/DataServices/IDbService.cs
@@ -0,0 +1,8 @@
+﻿namespace VismaMeeting_v2.Services.DataServices
+{
+    public interface IDbService<T>
+    {
+        void Save(T obj);
+        T Get();
+    }
+}
