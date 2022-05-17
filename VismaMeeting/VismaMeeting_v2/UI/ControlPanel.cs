393faaf25e002b5d9ff83c6c56f61f3960920890
vytautas-dotcom <tumas.vytautas@gmail.com>
2022-05-17 18:03:20 +0300

    Create ServiceCollection and add dependencies

---
+++ VismaMeeting/VismaMeeting_v2/UI/ControlPanel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VismaMeeting_v2.Services.DataDisplay;
+using VismaMeeting_v2.Services.DataOperations;
+using VismaMeeting_v2.Services.DataServices;
+using VismaMeeting_v2.Models;
+using VismaMeeting_v2.Commands;
+
+namespace VismaMeeting_v2.UI
+{
+    internal class ControlPanel
+    {
+        public IServiceProvider ConfigureServices()
+        {
+            var serviceCollection = new ServiceCollection();
+            return serviceCollection
+                
+                .AddSingleton<DataVisualization>()
+                .AddSingleton<DataCheck>()
+                .AddSingleton<IDbService<Persons>, DbService<Persons>>()
+                .AddSingleton<IDbService<Meetings>, DbService<Meetings>>()
+                .AddSingleton<CreateUser>()
+                .AddSingleton<FilterData>()
+                .AddSingleton<PersonMeetingData>()
+                .AddSingleton<PersonShowData>()
+                .AddSingleton<MeetingShowData>()
+                .AddSingleton<FilterShowData>()
+                .AddSingleton<MeetingsManagement>()
+                .AddSingleton<PersonsManagement>()
+                .AddSingleton<FilterManagement>()
+                .BuildServiceProvider();
+        }
+        
+    }
+}
