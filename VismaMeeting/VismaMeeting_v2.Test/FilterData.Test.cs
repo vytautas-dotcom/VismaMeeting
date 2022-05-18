a9406abc1842a7dfa77fbac4bcb227f3b1317e8e
vytautas-dotcom <tumas.vytautas@gmail.com>
2022-05-18 11:18:20 +0300

    First working test !!!!

--- VismaMeeting/VismaMeeting_v2.Test/FilterData.Test.cs
+++ VismaMeeting/VismaMeeting_v2.Test/FilterData.Test.cs
@@ -1,11 +1,52 @@
 using Xunit;
 using VismaMeeting_v2.Models;
 using VismaMeeting_v2.Services.DataDisplay;
+using System.Collections.Generic;
 
 namespace VismaMeeting_v2.Test
 {
     public class FilterDataTest
     {
+        
+        [Fact]
+        public void FilterByResponsiblePerson()
+        {
+            var person1 = new Person() { Name = "Tom" };
+            var person2 = new Person() { Name = "John" };
+            var person3 = new Person() { Name = "Tim" };
+
+            var persons = new Persons();
+            persons.AddRange(new[] { person1, person2, person3 });
+
+            var meeting1 = new Meeting();
+            meeting1.Persons = new List<Person> { person1 };
+            meeting1.ResponsiblePerson = person1.Name;
+
+            var meeting2 = new Meeting();
+            meeting2.Persons = new List<Person> { person1, person2 };
+            meeting2.ResponsiblePerson = person2.Name;
+
+            var meeting3 = new Meeting();
+            meeting3.Persons = new List<Person> { person1, person2, person3 };
+            meeting3.ResponsiblePerson = person3.Name;
+
+            var meetings1 = new Meetings();
+            meetings1.AddRange(new[] { meeting1 });
+
+            var meetings2 = new Meetings();
+            meetings2.AddRange(new[] { meeting1, meeting2 });
+
+            var meetings3 = new Meetings();
+            meetings3.AddRange(new[] { meeting1, meeting2, meeting3 });
+
+            var filter = new FilterData();
+
+            var result = filter.FilterByResponsiblePerson("Tom", meetings3, persons);
+            var expected = meetings1;
+
+            Assert.Equal(expected, result);
+
+        }
         [Fact]
         public void ShouldReturnMeeting1andMeeting2WhenGivenInterval2_0()
         {
@@ -33,10 +74,10 @@ namespace VismaMeeting_v2.Test
 
             var filter = new FilterData();
 
-            var result = filter.FilterByNumberOfAttendees((2, 0), meetings3);
-            var expected = new[] { meeting1, meeting2 };
+            var result = filter.FilterByNumberOfAttendees((0, 1), meetings3);
+            var expected = meetings3;
 
-            Assert.Equal(expected, result);
+            Assert.True(result.Contains(meeting1));
         }
     }
 }
